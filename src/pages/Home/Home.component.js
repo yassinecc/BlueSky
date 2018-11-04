df7994fb6dc73b6fead0ffadd6b78d9d065b8d6f
Yassine Chbani <yassinec@bam.tech>
2018-11-04 15:09:22 +0100

    Add time picker

--- src/pages/Home/Home.component.js
+++ src/pages/Home/Home.component.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Picker, Button } from 'react-native';
 
 import { Page } from 'BlueSky/src/components';
 
@@ -15,7 +15,14 @@ export default class Home extends Component<StateType> {
     super();
     const initialTime = Date.now();
     const startTime = 0;
-    this.state = { initialTime, timeLeft: startTime };
+    this.state = {
+      initialTime,
+      timeLeft: startTime,
+      chosenDate: new Date(),
+      chosenSeconds: 0,
+      chosenMinutes: 0,
+    };
+    this.secondsList = this.initSecondsList();
     setInterval(this.updateTimer, 1000);
   }
   props: PropsType;
@@ -27,10 +34,36 @@ export default class Home extends Component<StateType> {
     this.setState({ timeLeft: timeDifferenceInSeconds });
   };
 
+  setDate = date => this.setState({ chosenDate: date });
+
+  setMinutes = minutes => this.setState({ chosenMinutes: minutes });
+  setSeconds = seconds => this.setState({ chosenSeconds: seconds });
+
+  initSecondsList = () => {
+    const secondsArray = [];
+    for (var i = 0; i < 60; i++) {
+      secondsArray.push(<Picker.Item key={i} label={String(i)} value={i} />);
+    }
+    return secondsArray;
+  };
+
   render() {
     return (
       <Page>
         <View style={styles.container}>
+          <View style={{ flexDirection: 'row' }}>
+            <View style={{ flexGrow: 1 }}>
+              <Picker selectedValue={this.state.chosenMinutes} onValueChange={this.setMinutes}>
+                {this.secondsList}
+              </Picker>
+            </View>
+            <View style={{ flexGrow: 1 }}>
+              <Picker selectedValue={this.state.chosenSeconds} onValueChange={this.setSeconds}>
+                {this.secondsList}
+              </Picker>
+            </View>
+          </View>
+          <Button title="OK" onPress={this.setTimer} />
           <Text style={styles.welcome}>{this.state.timeLeft}</Text>
         </View>
       </Page>
@@ -50,7 +83,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    // alignItems: 'center',
   },
   welcome: {
     ...theme.fonts.header,
